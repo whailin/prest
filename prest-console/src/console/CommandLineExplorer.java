1f8c32bb0983a7d6c070c7f26d2c0d8b382d7892
boracaglayan <boracaglayan@gmail.com>
2011-02-11 17:42:35 +0000

    minor edit

--- prest-console/src/console/CommandLineExplorer.java
+++ prest-console/src/console/CommandLineExplorer.java
@@ -11,9 +11,9 @@ public class CommandLineExplorer {
 			packageExplorer.addNewProjectCmd(args[1]);
 		}
 		else if (args[0].equalsIgnoreCase("-parse")) {
-			if(args.length == 2)
+			if(args.length == 3)
 				packageExplorer.parseManualCmd(args[1], args[2], "");
-			if(args.length == 2)
+			if(args.length == 5)
 				packageExplorer.parseManualCmd(args[1], args[2], args[4]);
 		}
 		else if (args[0].equalsIgnoreCase("-logFilter")) {
