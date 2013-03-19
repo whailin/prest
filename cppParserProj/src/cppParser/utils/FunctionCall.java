c319b3bbde4a313bfcadd9c652e69cf9558b02e1
viisas.astelija@gmail.com <viisas.astelija@gmail.com@d88e06ec-ebdf-11dd-9c15-fdd50ba39c1e>
2013-03-19 18:38:13 +0000

    -Removed the old treeparser and dependencies to it in VarFinder -Added a frame for creating better function calls objects with strings that are extracted by FunctionAnalyzer

---
+++ cppParserProj/src/cppParser/utils/FunctionCall.java
@@ -0,0 +1,14 @@
+
+
+package cppParser.utils;
+
+/**
+ *
+ * @author Tomi
+ */
+public class FunctionCall {
+    public String[] owners;
+    public String name;
+    public FunctionCallParameter parameters;
+
+}
