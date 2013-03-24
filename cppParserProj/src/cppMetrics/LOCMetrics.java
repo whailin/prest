dc1496eafc06335147d528e7bfaa5a4d210e3914
trangiahoanglong <trangiahoanglong@gmail.com>
2013-03-24 14:19:20 +0000

    small change

--- cppParserProj/src/cppMetrics/LOCMetrics.java
+++ cppParserProj/src/cppMetrics/LOCMetrics.java
@@ -1,36 +1,46 @@
 package cppMetrics;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LOCMetrics implements Metrics {
 	private long cmtLines = 0;			//comment lines
-	private long logicalLOC = 0;		//logical lines of codes
-	private long physicalLOC = 0;		//physical lines of codes
-	private long LOC = 0;				//lines of codes
+	private int logicalLOC = 0;		//logical lines of codes
+	private int physicalLOC = 0;		//physical lines of codes
+	private int LOC = 0;				//lines of codes
 	
 	@Override
 	public void calculateMetrics() {
 		// TODO Auto-generated method stub
 	}
 
-	public LOCMetrics(long cmtL, long lloc, long ploc, long loc){
+	public LOCMetrics(long cmtL, int lloc, int ploc, int loc){
 		this.cmtLines = cmtL;
 		this.logicalLOC = lloc;
 		this.physicalLOC = ploc;
 		this.LOC = loc;
 	}
 	
+	public LOCMetrics(String file) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		while (br.readLine()!=null)
+			this.LOC ++;
+	}
+	
 	public long getCmtLines() {
 		return cmtLines;
 	}
 
-	public long getLloc() {
+	public int getLloc() {
 		return logicalLOC;
 	}
 
-	public long getPloc() {
+	public int getPloc() {
 		return physicalLOC;
 	}
 
-	public long getLoc() {
+	public int getLoc() {
 		return LOC;
 	}
 
