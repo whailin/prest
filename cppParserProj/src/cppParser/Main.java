5d5806022f0462df65d9ec8176fdff507fca00bf
viisas.astelija@gmail.com <viisas.astelija@gmail.com@d88e06ec-ebdf-11dd-9c15-fdd50ba39c1e>
2013-04-15 15:28:39 +0000

    -Added new commandline options -parse and -out -Directory names with spaces are now allowed when using -parse and/or -out if they are put in quotes

--- cppParserProj/src/cppParser/Main.java
+++ cppParserProj/src/cppParser/Main.java
@@ -7,11 +7,32 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+        switch(args.length){
+            case 0:
+                printInstructions();
+                break;
+            case 1:
+                Extractor e = new Extractor(args[0]);
+                e.process();
+                break;
+            default:
+                try{
+                    CmdLineParameterParser.parseParameters(args);
+                    System.out.println(CmdLineParameterParser.getInputDir()+" out:"+CmdLineParameterParser.getOutputDir());
+                }catch(Exception ex){
+                    System.out.println("Error:"+ex.getMessage());
+                    printInstructions();
+                }
 		// TODO Replace this main function with a call from Prest
+        
+        }
+        /*
 		
-		Extractor e = new Extractor(args[0]);
-		e.process();
-		
+		*/
 	}
+    
+    private static void printInstructions(){
+        System.out.println("Instructions");
+    }
 
 }
